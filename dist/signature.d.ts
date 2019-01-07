6752579d90cfea693225db2c7b057920a95b89fb
Bakr Baroudi <bakr@revenuemonster.my>
2019-01-07 14:45:32 +0800

    remove dist

--- dist/signature.d.ts
+++
@@ -1,9 +0,0 @@
-export declare function generateSignature(arg: {
-    data: any;
-    requestUrl: string;
-    nonceStr: string;
-    signType: string;
-    method: string;
-    timestamp: string;
-}, privateKey: string): string;
-//# sourceMappingURL=signature.d.ts.map
