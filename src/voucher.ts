12faa4677acd7fa9f0552235f9c6ad0fd34cf2ee
Bakr Baroudi <bakr@revenuemonster.my>
2019-01-08 09:48:55 +0800

    add voucher api

---
+++ src/voucher.ts
@@ -0,0 +1,128 @@
+import crypto = require('crypto')
+import { RMSDKInstance } from ".";
+import { generateSignature } from "./signature";
+
+export function issueVoucher(this: RMSDKInstance, accessToken: string, batchKey: string) {
+    const nonceStr = crypto.randomBytes(32).toString('hex')
+    const timestamp = new Date().getTime().toString()
+
+    return this.openApiInstance({
+        url: `voucher-batch/${batchKey}/issue`,
+        method: 'post',
+        headers: {
+            'Authorization': 'Bearer ' + accessToken,
+            'X-Timestamp': timestamp,
+            'X-Nonce-Str': nonceStr,
+            'X-Signature': 'sha256 ' + generateSignature({
+                data: null,
+                requestUrl: this.openApiUrl + `voucher-batch/${batchKey}/issue`,
+                nonceStr,
+                signType: 'sha256',
+                method: 'post',
+                timestamp,
+            }, this.privateKey)
+        }
+    })
+    .then(x => x.data)
+    .catch(err => console.error(err))
+}
+
+export function voidVoucher(this: RMSDKInstance, accessToken: string, code: string) {
+    const nonceStr = crypto.randomBytes(32).toString('hex')
+    const timestamp = new Date().getTime().toString()
+
+    return this.openApiInstance({
+        url: `voucher/${code}/issue`,
+        method: 'post',
+        headers: {
+            'Authorization': 'Bearer ' + accessToken,
+            'X-Timestamp': timestamp,
+            'X-Nonce-Str': nonceStr,
+            'X-Signature': 'sha256 ' + generateSignature({
+                data: null,
+                requestUrl: this.openApiUrl + `voucher/${code}/issue`,
+                nonceStr,
+                signType: 'sha256',
+                method: 'post',
+                timestamp,
+            }, this.privateKey)
+        }
+    })
+    .then(x => x.data)
+    .catch(err => console.error(err))
+}
+
+export function getVoucherByCode(this: RMSDKInstance, accessToken: string, code: string) {
+    const nonceStr = crypto.randomBytes(32).toString('hex')
+    const timestamp = new Date().getTime().toString()
+
+    return this.openApiInstance({
+        url: `voucher/${code}`,
+        method: 'get',
+        headers: {
+            'Authorization': 'Bearer ' + accessToken,
+            'X-Timestamp': timestamp,
+            'X-Nonce-Str': nonceStr,
+            'X-Signature': 'sha256 ' + generateSignature({
+                data: null,
+                requestUrl: this.openApiUrl + `voucher/${code}`,
+                nonceStr,
+                signType: 'sha256',
+                method: 'get',
+                timestamp,
+            }, this.privateKey)
+        }
+    })
+    .then(x => x.data)
+    .catch(err => console.error(err))
+}
+
+export function getVoucherBatches(this: RMSDKInstance, accessToken: string) {
+    const nonceStr = crypto.randomBytes(32).toString('hex')
+    const timestamp = new Date().getTime().toString()
+
+    return this.openApiInstance({
+        url: `voucher-batches`,
+        method: 'get',
+        headers: {
+            'Authorization': 'Bearer ' + accessToken,
+            'X-Timestamp': timestamp,
+            'X-Nonce-Str': nonceStr,
+            'X-Signature': 'sha256 ' + generateSignature({
+                data: null,
+                requestUrl: this.openApiUrl + `voucher-batches`,
+                nonceStr,
+                signType: 'sha256',
+                method: 'get',
+                timestamp,
+            }, this.privateKey)
+        }
+    })
+    .then(x => x.data)
+    .catch(err => console.error(err))
+}
+
+export function getVoucherBatcheByKey(this: RMSDKInstance, accessToken: string, batchKey: string) {
+    const nonceStr = crypto.randomBytes(32).toString('hex')
+    const timestamp = new Date().getTime().toString()
+
+    return this.openApiInstance({
+        url: `voucher-batches${batchKey}`,
+        method: 'get',
+        headers: {
+            'Authorization': 'Bearer ' + accessToken,
+            'X-Timestamp': timestamp,
+            'X-Nonce-Str': nonceStr,
+            'X-Signature': 'sha256 ' + generateSignature({
+                data: null,
+                requestUrl: this.openApiUrl + `voucher-batches${batchKey}`,
+                nonceStr,
+                signType: 'sha256',
+                method: 'get',
+                timestamp,
+            }, this.privateKey)
+        }
+    })
+    .then(x => x.data)
+    .catch(err => console.error(err))
+}
