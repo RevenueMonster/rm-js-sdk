6c901af0c80902469f06037bf16f39429a57c853
zypeh <zypeh.geek@gmail.com>
2019-01-08 15:06:15 +0800

    Update package.json

--- dist/payment.d.ts
+++
@@ -1,3 +0,0 @@
-import { RMSDKInstance } from ".";
-export declare function createTransactionUrl(this: RMSDKInstance, accessToken: string, data: object): Promise<any>;
-//# sourceMappingURL=payment.d.ts.map
