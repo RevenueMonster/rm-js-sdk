3bd8153da9ad56caa4dc54a4fb64031b1587b343
Desmond Hiew <desmond.hiew@techiesapp.io>
2023-05-12 15:52:37 +0900

    Fix signature. Added function to replace special character on json body

--- src/signature.ts
+++ src/signature.ts
@@ -1,45 +1,57 @@
 import crypto = require('crypto');
 
 export function sortObject(obj: any): any {
-    const sortedObj: any = {};
-    Object.keys(obj).sort().forEach((key): void => {
-        if(Array.isArray(obj[key])) {
-            sortedObj[key] = obj[key]
-        } else if(typeof obj[key] === 'object') {
-            sortedObj[key] = sortObject(obj[key]);
-        } else {
-            sortedObj[key] = obj[key];
-        }
+  const sortedObj: any = {};
+  Object.keys(obj)
+    .sort()
+    .forEach((key): void => {
+      if (Array.isArray(obj[key])) {
+        sortedObj[key] = obj[key];
+      } else if (typeof obj[key] === 'object') {
+        sortedObj[key] = sortObject(obj[key]);
+      } else {
+        sortedObj[key] = obj[key];
+      }
     });
-    return sortedObj;
+  return sortedObj;
 }
 
-export function generateSignature(arg: {
-    data: any,
-    requestUrl: string,
-    nonceStr: string,
-    signType: string,
-    method: string,
-    timestamp: string,
-}, privateKey: string): string {
+const replaceSpecialCharacters = (str: string) => {
+  return str
+    .replace(/</g, '\\u003c')
+    .replace(/>/g, '\\u003e')
+    .replace(/&/g, '\\u0026');
+};
 
-    let signature_data = ''
+export function generateSignature(
+  arg: {
+    data: any;
+    requestUrl: string;
+    nonceStr: string;
+    signType: string;
+    method: string;
+    timestamp: string;
+  },
+  privateKey: string
+): string {
+  let signature_data = '';
 
-    if (arg.data !== null && typeof arg.data === 'object') {
-        const signature_body = sortObject(arg.data)
-        const signature = Buffer.from(JSON.stringify(signature_body)).toString('base64')
-        signature_data = 'data=' + signature + '&'
-    }
+  if (arg.data !== null && typeof arg.data === 'object') {
+    const signature_body = sortObject(arg.data);
+    const jsonValue = replaceSpecialCharacters(JSON.stringify(signature_body));
+    const signature = Buffer.from(jsonValue).toString('base64');
+    signature_data = 'data=' + signature + '&';
+  }
 
-    const full_signature = signature_data
-    + 'method=' + arg.method
-    + '&nonceStr=' + arg.nonceStr
-    + '&requestUrl=' + arg.requestUrl
-    + '&signType=' + arg.signType
-    + '&timestamp=' + arg.timestamp
+  const full_signature = signature_data
+  + 'method=' + arg.method
+  + '&nonceStr=' + arg.nonceStr
+  + '&requestUrl=' + arg.requestUrl
+  + '&signType=' + arg.signType
+  + '&timestamp=' + arg.timestamp
 
-    return crypto
-        .createSign('SHA256')
-        .update(full_signature)
-        .sign(privateKey, 'base64')
+  return crypto
+    .createSign('SHA256')
+    .update(full_signature)
+    .sign(privateKey, 'base64');
 }
